a545e86e81e8389f83c94a40b9285e33d2cfc682
Jenks Guo <jenks.guo@xero.com>
2021-01-25 15:41:49 +1100

    Asset API Test skeleton created

--- Xero.NetStandard.OAuth2.Test/XeroConfiguration.cs
+++ Xero.NetStandard.OAuth2.Test/XeroConfiguration.cs
@@ -1,11 +1,12 @@
 
 using System;
 
-namespace Xero.NetStandard.OAuth2.Test.Model.Accounting
+namespace Xero.NetStandard.OAuth2.Test
 {
     public class XeroConfiguration
     {
         public string AccountingBaseUrl { get; set; }
         public string BankfeedsBaseUrl { get; set; }
+        public string AssetBaseUrl { get; set; }
     }
 }
